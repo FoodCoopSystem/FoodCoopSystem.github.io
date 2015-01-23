f91498a3c557c85b7eeb8d9b2b25abd5e83c4917
Karol Bryksa <kb@ratioweb.pl>
2015-01-23 14:21:57 +0100

    about team

--- js/global.js
+++ js/global.js
@@ -11,4 +11,32 @@ $(function() {
       }
     }
   });
+  
+  
+  // solutiona takan form http://jsfiddle.net/qvbgb/3/
+  var $people = $('.people'),
+  speed = 400,
+  easing = 'swing';
+  
+  $people
+    .find('.text')
+      .hide()
+      .end()
+    .hover(
+      function () {
+        $(this)
+          .find('.image')
+            .slideUp(speed, easing)
+            .end()
+          .find('.text')
+            .slideDown(speed, easing);
+      },function () {
+       $(this)
+        .find('.text')
+          .slideUp(speed, easing)
+          .end()
+        .find('.image')
+          .slideDown(speed, easing);
+      }
+    );
 });
